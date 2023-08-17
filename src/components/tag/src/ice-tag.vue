b2fea52ef29e5ca6c61180b7b40606eca9e93c85
张嘉凯 <killicestone@126.com>
2023-08-18 00:28:12 +0800

    feat:不知道修改了什么

---
+++ src/components/tag/src/ice-tag.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="ice-tag noselect" :class="[type,
+  round?'round':'defaultRound',
+  color?'btn-colors':'default-color',
+  size?`size-${size}`:'size-normal'
+  ]"
+       :style="{ '--hover-color': findColor(color).color,'--color': findColor(color).hover }"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { findColor } from '../../../hooks/tools.js'
+import { useRouter } from 'vue-router'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'normal'
+  },
+  round: {
+    type: Boolean,
+    default: false
+  },
+  color: {
+    type: String,
+    default: ''
+  },
+  size: {
+    type: String
+  },
+  href: {
+    type: String,
+    default: ''
+  }
+})
+
+const router = useRouter()
+const jump = () => {
+  if (props.href) {
+    const url = router.push({
+      path: props.href
+    })
+    // 设置在新页面打开
+    window.open(url, '_blank')
+  }
+}
+
+</script>
+<script>
+export default {
+  name: "iceTag"
+}
+</script>
+
+<style scoped lang="less">
+@import '../../../assets/variables.less';
+
+.ice-tag {
+  display: flex;
+  justify-content: center;
+  height: fit-content;
+  font-weight: bold;
+  z-index: 3;
+  position: relative;
+  width: fit-content;
+  border: none;
+  overflow: hidden;
+
+  &:before {
+    position: absolute;
+    content: '';
+    display: flex;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    transition-duration: @time-s;
+  }
+}
+
+.defaultRound {
+  border-radius: @radio-l;
+}
+
+// round
+.round {
+  border-radius: 50%;
+  aspect-ratio: 1;
+}
+
+// size\
+.size-small {
+  margin: @m-small;
+  padding: 0;
+  transition-duration: @time-s;
+}
+
+.size-normal {
+  margin: 0 @m-small;
+  padding: @p-small;
+  font-size: @fontSize-s;
+  transition-duration: @time-n;
+}
+
+.size-large {
+  font-size: @fontSize-l;
+  margin: @m-normal @m-large;
+  padding: @p-normal @p-large-su;
+  transition-duration: @time-l;
+}
+
+// 没有传入color时的颜色
+.default-color {
+  color: @themeColor;
+
+  &:before {
+    opacity: .1;
+    background: @themeColor;
+    transition-duration: @time-s;
+  }
+
+  &:hover {
+    border: none;
+    color: @themeColor-bleak;
+
+    &:before {
+      opacity: .3;
+      background: @themeColor;
+    }
+  }
+}
+
+// 传入color时的样式
+.btn-colors {
+  color: var(--color);
+  //border-color: var(--color);
+
+  &:before {
+    background: var(--color);
+    opacity: .2;
+  }
+
+  &:hover {
+    border: none;
+    color: var(--hover-color);
+    //border-color: var(--hover-color);
+
+    &:before {
+      opacity: .4;
+      background: var(--color);
+    }
+  }
+}
+</style>
