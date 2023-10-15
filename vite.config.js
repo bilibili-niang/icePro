029e73d4eae252fc2ab4f962558918e2ceaf9369
张嘉凯 <killicestone@126.com>
2023-10-16 00:42:34 +0800

    feat:修改了部分配置

--- vite.config.js
+++ vite.config.js
@@ -1,72 +1,81 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vitePluginVue from './plugins/fileFilter.js'
-import mdPlugin from "vite-plugin-markdown"
-import path from 'path'
-import eslintPlugin from 'vite-plugin-eslint'
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import vitePluginVue from "./plugins/fileFilter.js";
+import mdPlugin from "vite-plugin-markdown";
+import path from "path";
+import eslintPlugin from "vite-plugin-eslint";
 
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        mdPlugin.plugin({
-            mode: ["html", "vue"],
-        }),
-        vitePluginVue,
-        eslintPlugin({
-            include: ['src/**/*.ts', 'src/**/*.vue']
-        })
+  plugins: [
+    vue(),
+    mdPlugin.plugin({
+      mode: ["html", "vue"],
+    }),
+    vitePluginVue,
+    eslintPlugin({
+      include: ["src/**/*.ts", "src/**/*.vue"]
+    })
+  ],
+  output: {
+    globals: {
+      vue: "Vue"
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve("src"),
+    },
+    dedupe: [
+      "vue"
     ],
-    output: {
-        globals: {
-            vue: 'Vue'
-        }
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve('src'),
-        },
-        dedupe: [
-            'vue'
-        ],
-        extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-    },
-    css: {
-        // 预处理器配置项
-        preprocessorOptions: {
-            less: {
-                math: "always",
-                additionalData: "@import '@/assets/variables.less';"
-            },
-        }
-    },
-    /*build: {
-        lib: {
-            entry: "./index.js",
-            name: "icepro",
-            fileName: "icepro",
-            formats: ["es", "umd", "cjs"],
-        },
-        //打包后文件目录
-        outDir: "docs",
-        //压缩
-        minify: true,
-        external: ['vue'],
-        externals: ['vue'],
-        // 打包
-        rollupOptions: {
-            outDir: "docs",
-            base: '/docs/',
-            //压缩
-            minify: false,
-            // 排除 Vue 依赖
-            external: ['vue'],
-            inlineDynamicImports: true,
-            output: {
-                globals: {
-                    vue: 'Vue'
-                }
-            }
-        }
-    }*/
-})
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+  },
+  css: {
+    // 预处理器配置项
+    preprocessorOptions: {
+      less: {
+        math: "always",
+        additionalData: "@import '@/assets/variables.less';"
+      },
+    }
+  },
+  build: {
+    lib: {},
+    //打包后文件目录
+    outDir: "dist",
+    //压缩
+    minify: true,
+    external: ["vue"],
+    externals: ["vue"],
+  },
+  /*build: {
+      lib: {
+          entry: "./index.js",
+          name: "icepro",
+          fileName: "icepro",
+          formats: ["es", "umd", "cjs"],
+      },
+      //打包后文件目录
+      outDir: "docs",
+      //压缩
+      minify: true,
+      external: ['vue'],
+      externals: ['vue'],
+      // 打包
+      rollupOptions: {
+          outDir: "docs",
+          base: '/docs/',
+          //压缩
+          minify: false,
+          // 排除 Vue 依赖
+          external: ['vue'],
+          inlineDynamicImports: true,
+          output: {
+              globals: {
+                  vue: 'Vue'
+              }
+          }
+      }
+  }*/
+});
