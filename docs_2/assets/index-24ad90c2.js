f39e3157cd1c2460977c2887a479b54b2d21f46f
张嘉凯 <killicestone@126.com>
2023-10-24 15:00:23 +0800

    fix:尝试使其合法

--- docs_2/assets/index-24ad90c2.js
+++
@@ -1,8 +0,0 @@
-import{P as u}from"./preview-b5ce8146.js";import{_ as p,r as n,o as a,c as d,a as f,b as t,w as o,d as m,F as x,k as g}from"./index-61cb4129.js";function r(e){return e.__sourceCode=`<container>\r
-\r
-    <ice-pagination/>\r
-\r
-  </container>`,e.__sourceCodeTitle=" message-normal ",e.__styleCode=null,e.__script=`<script setup>\r
-\r
-\r
-<\/script>`,e}const s={},k={class:"ice-column"};function h(e,_){const c=n("ice-text"),i=n("ice-pagination"),l=n("container");return a(),d(x,null,[f("div",k,[t(c,null,{default:o(()=>[m(" 分页器 ")]),_:1})]),t(l,null,{default:o(()=>[t(i)]),_:1})],64)}typeof r=="function"&&r(s);const B=p(s,[["render",h]]),N={__name:"index",setup(e){return(_,c)=>(a(),g(u,{component:B}))}};export{N as default};
