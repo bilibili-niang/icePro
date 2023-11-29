785845fac92ea95acfd9bb69e15bcde71e4202f8
张嘉凯 <killicestone@126.com>
2023-11-29 19:49:35 +0800

    feat:修改弹窗组件的props

--- src/components/drawer/src/ice-drawer.vue
+++ src/components/drawer/src/ice-drawer.vue
@@ -4,9 +4,15 @@
     direction?direction:'left',
     modelValue?'fadeIn':'fadeOut'
   ]"
+         :style="{
+          'flex-direction':layoutDirection
+         }"
          class="ice-drawer"
     >
       <div :style="styleObj" class="slotStyle">
+        <ice-text>
+          {{ styleObj }}
+        </ice-text>
         <slot></slot>
       </div>
 
@@ -24,9 +30,9 @@
   </Teleport>
 </template>
 <script setup>
-import {computed, defineEmits, ref, watch, defineProps} from 'vue'
+import {computed, defineEmits, ref, watch, defineProps} from "vue";
 
-const emits = defineEmits(['update:modelValue'])
+const emits = defineEmits(["update:modelValue"]);
 const props = defineProps({
   // 如果不定义绑定的值的名称，默认为modelValue
   modelValue: Boolean,
@@ -34,68 +40,73 @@ const props = defineProps({
   value: String,
   width: {
     type: String,
-    default: '10rem'
+    default: "10rem"
   },
   direction: {
     type: String,
-    default: 'left'
+    default: "left"
   },
   percent: {
     type: String,
-    default: '20%'
+    default: "20%"
   }
-})
+});
 const close = () => {
-  emits('update:modelValue', false)
-}
+  emits("update:modelValue", false);
+};
 
 const styleObj = computed(() => {
-  if (props.direction === 'left' || props.direction === 'right') {
+  if (props.direction === "left" || props.direction === "right") {
     return {
       width: props.percent,
-    }
+    };
   } else {
     return {
       height: props.percent,
-    }
+    };
   }
-})
+});
+const layoutDirection = computed(() => {
+  if (props.direction === "left" || props.direction === "right") {
+    return "row";
+  } else {
+    return "column";
+  }
+});
 
-const body = ref(document.querySelector('body'))
+const body = ref(document.querySelector("body"));
 watch(() => props.modelValue, (newVal) => {
   if (newVal) {
-    body.value.classList.value += ' showDrawer'
+    body.value.classList.value += " showDrawer";
   } else {
-    body.value.classList.value = body.value.classList.value.replaceAll(' showDrawer', '')
+    body.value.classList.value = body.value.classList.value.replaceAll(" showDrawer", "");
   }
-})
+});
 </script>
 <script>
 export default {
   name: "ice-drawer"
-}
+};
 </script>
 
 <style lang="less" scoped>
 @import '../../../assets/variables.less';
 @import "../../../assets/animate.less";
 
-.ice-drawer {
+.ice-drawer{
   position: fixed;
   transition: all @time-l ease;
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction: row;
-  z-index: 50;
+  z-index: 17;
 
-  .blank {
+  .blank{
     flex: 1;
-    //filter: blur(5px);
     transition: all 1s ease;
   }
 
-  .slotStyle {
+  .slotStyle{
     height: 100%;
     background: @bac;
     border: @borderColor 1px solid;
@@ -103,43 +114,43 @@ export default {
 }
 
 // direction
-.left {
+.left{
   height: 100%;
   left: 0;
   top: 0;
 }
 
-.right {
+.right{
   height: 100%;
   right: 0;
   top: 0;
 }
 
-.top {
+.top{
   width: 100%;
   top: 0;
   left: 0;
 }
 
-.bottom {
+.bottom{
   width: 100%;
   bottom: 0;
   left: 0;
 }
 
-.fadeIn {
-  left: 0%;
+.fadeIn{
+  left: 0;
 }
 
-.fadeOut {
+.fadeOut{
   left: -100%;
 }
 
-@keyframes fadeIn {
-  0% {
+@keyframes fadeIn{
+  0%{
     opacity: 0;
   }
-  100% {
+  100%{
     opacity: 1;
   }
 }
--- src/pages/doc/drawer/drawer.preview.vue
+++ src/pages/doc/drawer/drawer.preview.vue
@@ -11,14 +11,23 @@
       <ice-text>
         你可以使用
         <ice-tag>percent</ice-tag>
-        传入指定宽度/高度,它的默认宽度/高度为20%
+        传入指定宽度/高度,它的默认宽度/高度为20%,请不要使用过小的百分比
       </ice-text>
+      <ice-column>
+        <ice-text>尝试修改
+          <ice-tag>width:</ice-tag>
+        </ice-text>
+        <ice-row>
+          <ice-input v-model="width"></ice-input>
+          <ice-button @click="percentWidth=width">修改</ice-button>
+        </ice-row>
+      </ice-column>
 
       <iceSplit/>
 
       <ice-selector v-model="direction" :list="selectionList"></ice-selector>
 
-      <ice-drawer v-model="drawer" :direction="direction" percent="30%">
+      <ice-drawer v-model="drawer" :direction="direction" :percent="percentWidth">
         <ice-text>
           drawer里面的数据
         </ice-text>
@@ -28,30 +37,32 @@
 
 </template>
 <script setup>
-import {reactive, ref} from 'vue'
+import {reactive, ref} from "vue";
 
 
-let direction = ref('left')
-let drawer = ref(false)
+let direction = ref("left");
+let drawer = ref(false);
 
+let percentWidth = ref("30%");
+let width = ref("20%");
 const selectionList = reactive([
   {
-    label: '从左侧展开',
-    value: 'left'
+    label: "从左侧展开",
+    value: "left"
   },
   {
-    label: '从右侧展开',
-    value: 'right'
+    label: "从右侧展开",
+    value: "right"
   },
   {
-    label: '从顶部展开',
-    value: 'top'
+    label: "从顶部展开",
+    value: "top"
   },
   {
-    label: '从底部展开',
-    value: 'bottom'
+    label: "从底部展开",
+    value: "bottom"
   }
-])
+]);
 
 </script>
 
