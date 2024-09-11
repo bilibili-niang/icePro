a2c3be7d5c7cdddbe8dc2ec08db053ef186421db
张嘉凯 <killicestone@126.com>
2024-09-11 16:17:25 +0800

    1

---
+++ docs/assets/index-048abbf4.js
@@ -0,0 +1,54 @@
+import{P as d}from"./preview-50ceab38.js";import{_ as f,r as i,o as l,c as s,b as e,w as c,d as n,F as _,a as h}from"./index-9145f539.js";import"https://cdn.skypack.dev/gsap@3.12.0";function x(t){return t.__sourceCode=`<container>\r
+    <ice-text>晴空一鹤排云上</ice-text>\r
+  </container>`,t.__sourceCodeTitle=" text-normal ",t.__styleCode=null,t.__script=null,t}const w={};function k(t,u){const o=i("ice-text"),r=i("container");return l(),s(_,null,[e(o,null,{default:c(()=>[n(" 最基础的 ")]),_:1}),e(r,null,{default:c(()=>[e(o,null,{default:c(()=>[n("晴空一鹤排云上")]),_:1})]),_:1})],64)}typeof x=="function"&&x(w);const P=f(w,[["render",k]]);function p(t){return t.__sourceCode=`<container>\r
+    <ice-text color="suxinhuang">晴空一鹤排云上</ice-text>\r
+    <ice-text color="eguanhong">晴空一鹤排云上</ice-text>\r
+    <ice-text color="wumeizi">晴空一鹤排云上</ice-text>\r
+    <ice-text color="ananlan">晴空一鹤排云上</ice-text>\r
+    <ice-text color="yaohuang">晴空一鹤排云上</ice-text>\r
+  </container>`,t.__sourceCodeTitle=" text-color ",t.__styleCode="<style lang='less' scoped></style>",t.__script=`<script setup>\r
+<\/script>`,t}const y={};function L(t,u){const o=i("ice-tag"),r=i("ice-text"),a=i("container");return l(),s(_,null,[e(r,null,{default:c(()=>[n(" 支持 "),e(o,null,{default:c(()=>[n("color")]),_:1}),n(" 的传入,默认支持 "),e(o,null,{default:c(()=>[n("hover")]),_:1}),n(" 动画 ")]),_:1}),e(a,null,{default:c(()=>[e(r,{color:"suxinhuang"},{default:c(()=>[n("晴空一鹤排云上")]),_:1}),e(r,{color:"eguanhong"},{default:c(()=>[n("晴空一鹤排云上")]),_:1}),e(r,{color:"wumeizi"},{default:c(()=>[n("晴空一鹤排云上")]),_:1}),e(r,{color:"ananlan"},{default:c(()=>[n("晴空一鹤排云上")]),_:1}),e(r,{color:"yaohuang"},{default:c(()=>[n("晴空一鹤排云上")]),_:1})]),_:1})],64)}typeof p=="function"&&p(y);const T=f(y,[["render",L]]);function g(t){return t.__sourceCode=`<container>\r
+    <div class="lim">\r
+      <ice-text>\r
+        overhead the albatross，hangs motionless upon the air，\r
+        and deep beneath the rolling waves，\r
+        in labyrinths of coral caves，an echo of a distant time，\r
+        comes willowing across the sand，and everything is green and\r
+        submarine.\r
+      </ice-text>\r
+      <ice-text nowrap>\r
+        overhead the albatross，hangs motionless upon the air，\r
+        and deep beneath the rolling waves，\r
+        in labyrinths of coral caves，an echo of a distant time，\r
+        comes willowing across the sand，and everything is green and\r
+        submarine.\r
+      </ice-text>\r
+    </div>\r
+  </container>`,t.__sourceCodeTitle=" text-nowrap ",t.__styleCode=`<style lang='less' scoped>\r
+.lim {\r
+  width: 600px;\r
+  overflow: hidden;\r
+}\r
+</style>`,t.__script=null,t}const b={},B={class:"lim"};function N(t,u){const o=i("ice-tag"),r=i("ice-text"),a=i("container");return l(),s(_,null,[e(r,null,{default:c(()=>[n(" 默认换行, "),e(o,null,{default:c(()=>[n("nowrap")]),_:1}),n(" 禁止换行 ")]),_:1}),e(a,null,{default:c(()=>[h("div",B,[e(r,null,{default:c(()=>[n(" overhead the albatross，hangs motionless upon the air， and deep beneath the rolling waves， in labyrinths of coral caves，an echo of a distant time， comes willowing across the sand，and everything is green and submarine. ")]),_:1}),e(r,{nowrap:""},{default:c(()=>[n(" overhead the albatross，hangs motionless upon the air， and deep beneath the rolling waves， in labyrinths of coral caves，an echo of a distant time， comes willowing across the sand，and everything is green and submarine. ")]),_:1})])]),_:1})],64)}typeof g=="function"&&g(b);const V=f(b,[["render",N],["__scopeId","data-v-0ff7a3ab"]]);function v(t){return t.__sourceCode=`<container>\r
+    <ice-text>晴空一鹤排云上</ice-text>\r
+    <ice-text size="s">晴空一鹤排云上</ice-text>\r
+    <ice-text size="l">晴空一鹤排云上</ice-text>\r
+  </container>`,t.__sourceCodeTitle=" text-size ",t.__styleCode="<style lang='less' scoped></style>",t.__script=`<script setup>\r
+\r
+<\/script>`,t}const $={},F={class:"ice-column"};function I(t,u){const o=i("ice-tag"),r=i("ice-text"),a=i("container");return l(),s(_,null,[h("div",F,[e(r,null,{default:c(()=>[n(" 支持 "),e(o,null,{default:c(()=>[n("size")]),_:1}),n(" 的传入,默认为 "),e(o,null,{default:c(()=>[n("n")]),_:1}),n(" ,可选值有 "),e(o,null,{default:c(()=>[n("s")]),_:1}),n(" , "),e(o,null,{default:c(()=>[n("l")]),_:1})]),_:1})]),e(a,null,{default:c(()=>[e(r,null,{default:c(()=>[n("晴空一鹤排云上")]),_:1}),e(r,{size:"s"},{default:c(()=>[n("晴空一鹤排云上")]),_:1}),e(r,{size:"l"},{default:c(()=>[n("晴空一鹤排云上")]),_:1})]),_:1})],64)}typeof v=="function"&&v($);const U=f($,[["render",I]]);function m(t){return t.__sourceCode=`<container>\r
+    <div class="lim">\r
+      <ice-text underLine>\r
+        晴空一鹤排云上\r
+      </ice-text>\r
+      <ice-text color="tieshuihong" underLine>\r
+        晴空一鹤排云上\r
+      </ice-text>\r
+    </div>\r
+  </container>`,t.__sourceCodeTitle=" text-underLine ",t.__styleCode=`<style lang='less' scoped>\r
+.lim {\r
+  width: 600px;\r
+  overflow: hidden;\r
+}\r
+\r
+</style>`,t.__script=`<script setup>\r
+<\/script>`,t}const z={},E={class:"lim"};function S(t,u){const o=i("ice-tag"),r=i("ice-text"),a=i("container");return l(),s(_,null,[e(r,null,{default:c(()=>[n(" 支持 "),e(o,null,{default:c(()=>[n("underLine")]),_:1}),n(" 的传入 , 默认为 "),e(o,null,{default:c(()=>[n("false")]),_:1}),n(" , 当然也可以传入 "),e(o,null,{default:c(()=>[n("color")]),_:1}),n(" 控制它的颜色 ")]),_:1}),e(a,null,{default:c(()=>[h("div",E,[e(r,{underLine:""},{default:c(()=>[n(" 晴空一鹤排云上 ")]),_:1}),e(r,{color:"tieshuihong",underLine:""},{default:c(()=>[n(" 晴空一鹤排云上 ")]),_:1})])]),_:1})],64)}typeof m=="function"&&m(z);const W=f(z,[["render",S],["__scopeId","data-v-2f05399d"]]),D={__name:"index",setup(t){return(u,o)=>(l(),s(_,null,[e(d,{component:P}),e(d,{component:T}),e(d,{component:V}),e(d,{component:U}),e(d,{component:W})],64))}};export{D as default};
---
+++ docs/assets/index-55bb5dc4.js
@@ -0,0 +1,39 @@
+import{P as y}from"./preview-50ceab38.js";import{i as u,r as p,o as h,c as k,a as n,b as l,w as c,d as _,u as r,A as v,t as B,F as I,_ as S,C as U,D as j,l as N}from"./index-9145f539.js";import"https://cdn.skypack.dev/gsap@3.12.0";function b(e){return e.__sourceCode=`<container>\r
+    <div class="ice-column">\r
+      <div class="ice-row">\r
+        <ice-input v-model="text" class="input" placeholder="请输入文字"></ice-input>\r
+        <ice-text>\r
+          我的名字:{{ text }}\r
+        </ice-text>\r
+      </div>\r
+      <br />\r
+      <div class="ice-row">\r
+        <ice-input v-model="text2" class="input" placeholder="请输入文字"></ice-input>\r
+      </div>\r
+      <br />\r
+      <ice-text>\r
+        禁用\r
+        <ice-tag>disabled</ice-tag>\r
+        <br />\r
+      </ice-text>\r
+      <div class="ice-row">\r
+        <ice-input v-model="text3" class="input" disabled placeholder="请输入文字"></ice-input>\r
+      </div>\r
+      <div class="ice-row">\r
+        <ice-input v-model="obj.text" class="input" placeholder="对象属性修改"></ice-input>\r
+      </div>\r
+    </div>\r
+  </container>`,e.__sourceCodeTitle=" input-normal ",e.__styleCode=`<style lang="less" scoped>\r
+.input {\r
+  margin-right: 1rem;\r
+}\r
+</style>`,e.__script=`<script setup>\r
+import { ref } from 'vue'\r
+\r
+let text = ref('坤坤菜菜子')\r
+let text2 = ref('一个真正的鳗')\r
+let text3 = ref('iKun铠甲,合体!')\r
+let obj = ref({\r
+  text: '测试文字'\r
+})\r
+<\/script>`,e}const x=e=>(U("data-v-be9c8fe4"),e=e(),j(),e),P={class:"ice-column"},C={class:"ice-column"},D={class:"ice-row"},F=x(()=>n("br",null,null,-1)),K={class:"ice-row"},T=x(()=>n("br",null,null,-1)),A=x(()=>n("br",null,null,-1)),E={class:"ice-row"},R={class:"ice-row"},w={__name:"input.preview",setup(e){let o=u("坤坤菜菜子"),s=u("一个真正的鳗"),a=u("iKun铠甲,合体!"),f=u({text:"测试文字"});return(z,i)=>{const m=p("ice-text"),d=p("ice-input"),V=p("ice-tag"),g=p("container");return h(),k(I,null,[n("div",P,[l(m,null,{default:c(()=>[_(" 最基础的使用 ")]),_:1})]),l(g,null,{default:c(()=>[n("div",C,[n("div",D,[l(d,{modelValue:r(o),"onUpdate:modelValue":i[0]||(i[0]=t=>v(o)?o.value=t:o=t),class:"input",placeholder:"请输入文字"},null,8,["modelValue"]),l(m,null,{default:c(()=>[_(" 我的名字:"+B(r(o)),1)]),_:1})]),F,n("div",K,[l(d,{modelValue:r(s),"onUpdate:modelValue":i[1]||(i[1]=t=>v(s)?s.value=t:s=t),class:"input",placeholder:"请输入文字"},null,8,["modelValue"])]),T,l(m,null,{default:c(()=>[_(" 禁用 "),l(V,null,{default:c(()=>[_("disabled")]),_:1}),A]),_:1}),n("div",E,[l(d,{modelValue:r(a),"onUpdate:modelValue":i[2]||(i[2]=t=>v(a)?a.value=t:a=t),class:"input",disabled:"",placeholder:"请输入文字"},null,8,["modelValue"])]),n("div",R,[l(d,{modelValue:r(f).text,"onUpdate:modelValue":i[3]||(i[3]=t=>r(f).text=t),class:"input",placeholder:"对象属性修改"},null,8,["modelValue"])])])]),_:1})],64)}}};typeof b=="function"&&b(w);const q=S(w,[["__scopeId","data-v-be9c8fe4"]]),L={__name:"index",setup(e){return(o,s)=>(h(),N(y,{component:q}))}};export{L as default};
