ef471e0ccc1288b9dd39d407acabb57d58cfe1ee
张嘉凯 <killicestone@126.com>
2024-09-11 16:17:11 +0800

    1

--- docs/assets/tag.preview-ac1f6a3b.js
+++
@@ -1,6 +0,0 @@
-import{_ as s,r as n,o as d,c as f,a as g,b as e,w as t,d as a,F as h}from"./index-e7d85970.js";function l(c){return c.__sourceCode=`<container class="ice-column">\r
-    <ice-tag>default</ice-tag>\r
-    <ice-tag color="pipahuang">default</ice-tag>\r
-    <ice-tag color="wanshoujuhuang">default</ice-tag>\r
-    <ice-tag color="dahong">default</ice-tag>\r
-  </container>`,c.__sourceCodeTitle=" tag ",c.__styleCode=null,c.__script=null,c}const r={},p={class:"ice-column"};function m(c,x){const u=n("iceTag"),_=n("ice-text"),o=n("ice-tag"),i=n("container");return d(),f(h,null,[g("div",p,[e(_,null,{default:t(()=>[a(" 使用 "),e(u,null,{default:t(()=>[a("color")]),_:1}),a(" 来控制颜色类型 ")]),_:1})]),e(i,{class:"ice-column"},{default:t(()=>[e(o,null,{default:t(()=>[a("default")]),_:1}),e(o,{color:"pipahuang"},{default:t(()=>[a("default")]),_:1}),e(o,{color:"wanshoujuhuang"},{default:t(()=>[a("default")]),_:1}),e(o,{color:"dahong"},{default:t(()=>[a("default")]),_:1})]),_:1})],64)}typeof l=="function"&&l(r);const T=s(r,[["render",m]]);export{T as t};
