068aa1599472bf1fe77fb58216197bb33bd19dc7
张嘉凯 <killicestone@126.com>
2024-09-11 15:44:00 +0800

    feat: :sparkles: 修改打包配置,命令

--- src/router/index.js
+++ src/router/index.js
@@ -1,124 +1,125 @@
-import {createRouter, createWebHistory} from "vue-router"
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
-    path: "/",
-    name: "index",
-    component: () => import("../pages/index.vue")
+    path: '/',
+    name: 'index',
+    component: () => import('../pages/index.vue')
   },
   {
-    path: "/doc",
-    component: () => import("../pages/Doc.vue"),
-    redirect: "/doc/button",
+    path: '/doc',
+    component: () => import('../pages/Doc.vue'),
+    redirect: '/doc/button',
     children: [
       {
-        path: "button",
-        name: "button按钮",
-        component: () => import("../pages/doc/button/index.vue")
+        path: 'button',
+        name: 'button按钮',
+        component: () => import('../pages/doc/button/index.vue')
       },
       {
-        path: "button/colors",
-        name: "colors",
-        component: () => import("../pages/doc/button/colors.vue")
+        path: 'button/colors',
+        name: 'colors',
+        component: () => import('../pages/doc/button/colors.vue')
       },
       {
-        path: "tools",
-        name: "工具",
-        component: () => import("../pages/doc/tools/index.vue"),
+        path: 'tools',
+        name: '工具',
+        component: () => import('../pages/doc/tools/index.vue')
       },
       {
-        path: "card",
-        component: () => import("../pages/doc/card/index.vue"),
+        path: 'card',
+        component: () => import('../pages/doc/card/index.vue')
       },
       {
-        path: "split",
-        component: () => import("../pages/doc/split/index.vue"),
+        path: 'split',
+        component: () => import('../pages/doc/split/index.vue')
       },
       {
-        path: "text",
-        component: () => import("../pages/doc/text/index.vue"),
+        path: 'text',
+        component: () => import('../pages/doc/text/index.vue')
       },
       {
-        path: "message",
-        component: () => import("../pages/doc/message/index.vue"),
+        path: 'message',
+        component: () => import('../pages/doc/message/index.vue')
       },
       {
-        path: "iceMessageBox",
-        component: () => import("../pages/doc/iceMessageBox/index.vue"),
+        path: 'iceMessageBox',
+        component: () => import('../pages/doc/iceMessageBox/index.vue')
       },
       {
-        path: "drawer",
-        component: () => import("../pages/doc/drawer/index.vue"),
+        path: 'drawer',
+        component: () => import('../pages/doc/drawer/index.vue')
       },
       {
-        path: "menu",
-        component: () => import("../pages/doc/menu/index.vue"),
+        path: 'menu',
+        component: () => import('../pages/doc/menu/index.vue')
       },
       {
-        path: "input",
-        component: () => import("../pages/doc/input/index.vue"),
+        path: 'input',
+        component: () => import('../pages/doc/input/index.vue')
       },
       {
-        path: "textarea",
-        component: () => import("../pages/doc/textarea/index.vue"),
+        path: 'textarea',
+        component: () => import('../pages/doc/textarea/index.vue')
       },
       {
-        path: "container",
-        component: () => import("../pages/doc/container/index.vue"),
+        path: 'container',
+        component: () => import('../pages/doc/container/index.vue')
       }, {
-        path: "tag",
-        component: () => import("../pages/doc/tag/index.vue"),
+        path: 'tag',
+        component: () => import('../pages/doc/tag/index.vue')
       }, {
-        path: "avatar",
-        component: () => import("../pages/doc/avatar/index.vue"),
+        path: 'avatar',
+        component: () => import('../pages/doc/avatar/index.vue')
       },
       {
-        path: "shrinkBar",
-        component: () => import("../pages/doc/shrinkBar/index.vue"),
+        path: 'shrinkBar',
+        component: () => import('../pages/doc/shrinkBar/index.vue')
       },
       {
-        path: "link",
-        component: () => import("../pages/doc/link/index.vue"),
+        path: 'link',
+        component: () => import('../pages/doc/link/index.vue')
       },
       {
-        path: "header",
-        component: () => import("../pages/doc/header/index.vue"),
+        path: 'header',
+        component: () => import('../pages/doc/header/index.vue')
       },
       {
-        path: "colorSelector",
-        component: () => import("../pages/doc/colorSelector/index.vue"),
+        path: 'colorSelector',
+        component: () => import('../pages/doc/colorSelector/index.vue')
       },
       {
-        path: "selector",
-        component: () => import("../pages/doc/selector/index.vue"),
+        path: 'selector',
+        component: () => import('../pages/doc/selector/index.vue')
       },
       {
-        path: "pagination",
-        component: () => import("../pages/doc/pagination/index.vue"),
+        path: 'pagination',
+        component: () => import('../pages/doc/pagination/index.vue')
       },
       {
-        path: "imgPreview",
-        component: () => import("../pages/doc/iceImgPreview/index.vue"),
+        path: 'imgPreview',
+        component: () => import('../pages/doc/iceImgPreview/index.vue')
       },
       {
-        path: "inlineStyle",
-        component: () => import("../pages/doc/inlineStyle/index.vue"),
+        path: 'inlineStyle',
+        component: () => import('../pages/doc/inlineStyle/index.vue')
       },
       {
-        path: "tabs",
-        component: () => import("../pages/doc/tabs/index.vue"),
+        path: 'tabs',
+        component: () => import('../pages/doc/tabs/index.vue')
       },
       {
-        path: "dragAndDrop",
-        component: () => import("../pages/doc/dragAndDrop/index.vue"),
+        path: 'dragAndDrop',
+        component: () => import('../pages/doc/dragAndDrop/index.vue')
       }
-    ],
-  },
+    ]
+  }
 ]
 
 const router = createRouter({
-  history: createWebHistory("/"),
-  routes,
+  // history: createWebHistory("/"),
+  history: createWebHashHistory('/'),
+  routes
 })
 
 export default router
--- vite.config.js
+++ vite.config.js
@@ -1,69 +1,83 @@
-import {defineConfig} from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // 这个插件在开发时必须存在
-import vitePluginVue from "./plugins/fileFilter.js"
-import mdPlugin from "vite-plugin-markdown"
-import path from "path"
-import eslintPlugin from "vite-plugin-eslint"
+import vitePluginVue from './plugins/fileFilter.js'
+import mdPlugin from 'vite-plugin-markdown'
+import path from 'path'
+import eslintPlugin from 'vite-plugin-eslint'
 
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        mdPlugin.plugin({
-            mode: ["html", "vue"],
-        }),
-        vitePluginVue,
-        // inlineStyleTransform,
-        eslintPlugin({
-            include: ["src/**/*.js", "src/**/*.vue"]
-        })
+  base: './',
+  sourceMap: true,
+  plugins: [
+    vue(),
+    mdPlugin.plugin({
+      mode: ['html', 'vue']
+    }),
+    vitePluginVue,
+    // inlineStyleTransform,
+    eslintPlugin({
+      include: ['src/**/*.js', 'src/**/*.vue']
+    })
+  ],
+  output: {
+    // 设置打包后的文件目录
+    dir: 'dist/',
+    // 外部化 Vue，这样它就不会被打包到最终的 JavaScript 文件中
+    external: ['vue']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('src')
+    },
+    dedupe: [
+      'vue'
     ],
-    output: {
-        // 设置打包后的文件目录
-        dir: "dist/",
-        // 外部化 Vue，这样它就不会被打包到最终的 JavaScript 文件中
-        external: ["vue"],
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve("src"),
-        },
-        dedupe: [
-            "vue"
-        ],
-        extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-    },
-    css: {
-        // 预处理器配置项
-        preprocessorOptions: {
-            less: {
-                math: "always",
-                additionalData: "@import '@/assets/variables.less';"
-            },
-        }
-    },
-    build: {
-        //打包后文件目录
-        outDir: "es",
-        //压缩
-        minify: true,
-        rollupOptions: {
-            //忽略打包vue文件
-            external: ["vue"],
-            output: {
-                globals: {
-                    vue: "Vue",
-                },
-                dir: "dist",
-            },
-        },
-        lib: {
-            entry: "./index.js",
-            name: "icepro",
-            fileName: "icepro",
-            formats: ["es", "umd", "cjs"],
-        },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  },
+  css: {
+    // 预处理器配置项
+    preprocessorOptions: {
+      less: {
+        math: 'always',
+        additionalData: '@import \'@/assets/variables.less\';'
+      }
     }
+  },
+  build2: {
+    //打包后文件目录
+    outDir: 'es',
+    //压缩
+    minify: true,
+    rollupOptions: {
+      //忽略打包vue文件
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        },
+        dir: 'dist'
+      }
+    },
+    lib: {
+      entry: './index.js',
+      name: 'icepro',
+      fileName: 'icepro',
+      formats: ['es', 'umd', 'cjs']
+    }
+  },
+  build: {
+    outDir: 'dist', // 指定构建输出目录
+    minify: true, // 启用代码压缩
+    rollupOptions: {
+      // external: ['vue'],
+      /*output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }*/
+    }
+  }
 })
