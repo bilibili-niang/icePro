7c864cc8cd491e8df8c0cb7c4812bf4095a250db
张嘉凯 <killicestone@126.com>
2025-03-04 10:20:46 +0800

    feat(colorSelector): 重构颜色选择器组件
    - 新增展开和收起功能，优化用户体验
    - 添加主题切换和色阶切换动画
    - 调整样式，增加视觉一致性
    - 优化代码结构，提高可维护性

--- src/pages/doc/colorSelector/colorSelector.normal.preview.vue
+++ src/pages/doc/colorSelector/colorSelector.normal.preview.vue
@@ -2,18 +2,20 @@
 color-selector
 </preview>
 <template>
-  <ice-text>默认使用方法</ice-text>
-  <container>
-    <ice-column>
-      <ice-text>
-        colorValue: {{ colorValue }}
-      </ice-text>
-      <color-selector v-model="colorValue"></color-selector>
-    </ice-column>
-  </container>
+  <div class="color-selector-preview">
+    <ice-text>默认使用方法</ice-text>
+    <container>
+      <ice-column>
+        <ice-text>
+          colorValue: {{ colorValue }}
+        </ice-text>
+        <color-selector v-model="colorValue"></color-selector>
+      </ice-column>
+    </container>
+  </div>
 </template>
 <script setup>
 import {ref} from "vue"
-
+import ColorSelector from "@/components/colorSelector"
 const colorValue = ref("")
 </script>
